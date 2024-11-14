74850e467148d566b78ced9e40967eecd7a31fad
romanchick1230 <romangemini2010@gmail.com>
2024-11-14 19:48:17 +0300

    sec upd

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,1 +1,20 @@
-from aiogram.types import ReplyKeyboardMarkupp, KeyboardButton
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
+                           InlineKeyboardMarkup, InlineKeyboardButton)
+
+main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Каталог')],
+                                     [KeyboardButton(text='Корзина')],
+                                     [KeyboardButton(text='Контакты'),
+                                      KeyboardButton(text='О нас')]],
+                           resize_keyboard=True,
+                           input_field_placeholder='Выберите пунк меню...')
+
+
+catalog = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Футболки', callback_data='t-shirt')],
+    [InlineKeyboardButton(text='Красовки', callback_data='sneakers')],
+    [InlineKeyboardButton(text='Кепки', callback_data='cap')]])
+
+
+get_number = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Отправить номер',
+                                                           request_contact=True)]],
+                                 resize_keyboard=True)
